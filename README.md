7c7074165d3980632f19fd7d52429052b42a8670
Jeffrey Tong <jeffrey.w.tong@gmail.com>
2024-11-06 13:01:49 -0800

    Finish MWE of game logic

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ pip install -r requirements.txt
 ```
 
 ## Running
+Tested with Python 3.12.4.
 ```
 python3 main.py
 ```
--- main.py
+++ main.py
@@ -1,16 +1,36 @@
-# Example file showing a circle moving on screen
 import pygame
+
+WIDTH, HEIGHT = 600, 600
+INITIAL_PELLETS = set([
+    (30, 60),
+    (120, 50),
+    (80, 170),
+    (240, 350),
+    (270, 530)
+])
+PELLET_RADIUS = 10.0
+GHOST_COLLISION_MARGIN = 10.0
+FONT_FAMILY = './assets/PressStart2P-Regular.ttf'
+FONT_SIZE = 20
+GHOST_SPEED = 3.0 # In px/frame
+PLAYER_RADIUS = 20.0
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+FONT = pygame.font.Font('./assets/PressStart2P-Regular.ttf', FONT_SIZE)
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
-running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+pellets = INITIAL_PELLETS.copy()
+score = 0
+state = 'RUNNING'
 
-while running:
+# TODO replace with the ghost pos from pose estimation
+pacman_pos = pygame.math.Vector2(0, 0)
+ghost_pos = pygame.math.Vector2(WIDTH / 2, HEIGHT / 2)
+
+while True:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -18,19 +38,31 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill('black')
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
+    if state == 'RUNNING':
+        pacman_pos = pygame.Vector2(pygame.mouse.get_pos())
+        ghost_pos.move_towards_ip(pacman_pos, GHOST_SPEED)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    pygame.draw.circle(screen, "yellow", pacman_pos, PLAYER_RADIUS)
+    
+    # Need to copy to avoid "set changed size during iteration" error
+    for pellet in pellets.copy():
+        if pacman_pos.distance_to(pellet) < PELLET_RADIUS + PLAYER_RADIUS:
+            pellets.remove(pellet)
+            score += 1
+        pygame.draw.circle(screen, "yellow", pellet, PELLET_RADIUS)
+
+    pygame.draw.circle(screen, "red", ghost_pos, PLAYER_RADIUS)
+
+    if len(pellets) == 0:
+        state = 'YOU WON!'
+    elif pacman_pos.distance_to(ghost_pos) < PLAYER_RADIUS * 2 + GHOST_COLLISION_MARGIN:
+        state = 'YOU LOST'
+
+    screen.blit(FONT.render(f'SCORE: {score}', True, 'white'), (100, 20))
+    if state != 'RUNNING':
+        screen.blit(FONT.render(state, True, 'white'), (WIDTH / 2, HEIGHT / 2))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
@@ -39,5 +71,3 @@ while running:
     # dt is delta time in seconds since last frame, used for framerate-
     # independent physics.
     dt = clock.tick(60) / 1000
-
-pygame.quit()
